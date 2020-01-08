2b5ba7050a120190d30f031432b0d802790b19ec
voidtek <vdacosta@voidtek.com>
2020-01-08 12:01:19 +0000

    Use httrack cmd.

--- mirror-offline-docker/mirror.sh
+++ mirror-offline-docker/mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf /usr/share/nginx/html/*
-wget -P /tmp -mpck --user-agent="" -e robots=off -R js,gif,jpg,jpeg,png,pdf,ppt,GIF,JPG,JPEG,PNG,PDF,PPT -E ${PLUGIN_WEBSITE}
-# wget -P /tmp -mpck --user-agent="" -e robots=off -E ${PLUGIN_WEBSITE}
-mv -f /tmp/${PLUGIN_FOLDER}/* /usr/share/nginx/html
+httrack ${PLUGIN_WEBSITE} --path "/usr/share/nginx/html/" \
+   -*.css \
+   -*.js \
+   --verbose
